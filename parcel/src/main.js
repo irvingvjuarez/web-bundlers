d7a1ac53f5fec17e94a47f4ebff5d2ab9f3f870d
Irving Juárez <irvingjuarez274@gmail.com>
2023-05-19 20:47:36 -0500

    [ADD] js code into parcel project

--- parcel/src/main.js
+++ parcel/src/main.js
@@ -3,3 +3,25 @@ const LIMIT = 10
 
 const $app = document.getElementById("app")
 const API = `https://api.escuelajs.co/api/v1/products?offset=${OFFSET}&limit=${LIMIT}`
+
+async function main() {
+  const response = await fetch(API)
+  const products = await response.json()
+
+  const output = products.map(product => `
+  <article class="Card">
+    <img src="${product.images[0]}" />
+    <h2>
+      ${product.title} <small>Precio $${product.price}</small>
+    </h2>
+  </article>
+  `).join("")
+
+  const cardsWrapper = document.createElement("section")
+  cardsWrapper.classList.add("Items")
+  cardsWrapper.innerHTML = output
+
+  $app.appendChild(cardsWrapper)
+}
+
+main()
