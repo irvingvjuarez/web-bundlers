2c34b10b913986ae7cc0c3eada6062b608ebc47f
Irving Juárez <irvingjuarez274@gmail.com>
2023-05-24 18:57:26 -0600

    [ADD] esbuild with the build and dev commands

---
+++ esbuild/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './components/App.tsx';
+
+ReactDOM.render(
+    <App message = 'Practica de ESBUILD'/>,
+    document.getElementById('app')
+)
